3f30f3315e94ad3b48dc81e4266ef6b75596c917
Abdularahim Al Balushi <143422547+ARahim900@users.noreply.github.com>
2025-07-18 15:18:21 +0400

    Add temporary diagnostic file to check R.F. Age page issue

---
+++ src/diagnostic-temp.ts
@@ -0,0 +1,6 @@
+// Temporary diagnostic file to understand project structure
+// This file will be removed after fixing the issue
+
+console.log("Checking project structure for R.F. Age page...");
+
+export {};
